53a9fd2c57fdb5fac15e1ccf6bc5a4f021e95601
RSConcordia <renansconcordia@gmail.com>
2015-10-13 16:26:15 -0300

    -'+'

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
 					bluetoothSerial.list(function(devices){
 						devices.forEach(function(device){									
 									var lista = "";
-									lista += "<a href='#' onclick='connect()'>"+device.id+"</a><br>";
+									lista = "<a href='#' onclick='connect()'>"+device.id+"</a><br>";
 									document.getElementById("dispositivos").innerHTML = lista;
 								})
 							}, alert('Error'));
