795cbb1107440029a7ace33facb2cbc2d8985d24
RSConcordia <renansconcordia@gmail.com>
2015-10-13 13:03:26 -0300

    teste001

--- js/index.js
+++ js/index.js
@@ -4,46 +4,10 @@
 	function connect(){		
 				alert('#1-Connect');
 				
-						
-		//Opens Bluetooth.
-		BC.Bluetooth.OpenBluetooth(function()
-									{
-										alert("bluetooth opened!");
-									},
-									function()
-									{
-										alert("bluetooth open error!");
-									});
-				alert('#2-Connect');
+					bluetoothSerial.list(function(devices){
+						devices.forEach(function(device){
+									alert(device.id);
+									})
+									},alert('Erro'););
+									
 	};
-	function lista(){		
-				alert('#1-Lista');
-				
-		BC.Bluetooth.GetConnectedDevices(function(mes)
-											{
-											alert(JSON.stringify(mes));
-											});
-				alert('#2-Lista');
-	};
-	function desconectar(){		
-				alert('#1-Desconectar');
-				
-		BC.Bluetooth.CloseBluetooth(function()
-									{
-									alert("bluetooth closed!");
-									},
-									function()
-									{
-									alert("bluetooth close error!");
-									});
-				alert('#2-Desconectar');
-			};
-	function pareados(){		
-				alert('#1-Pareados');
-		BC.Bluetooth.GetPairedDevices(function(mes)
-										{
-										alert(JSON.stringify(mes));
-										});
-				alert('#2-Pareados');
-			
-			};
