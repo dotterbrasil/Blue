9611415e48f3e9f2915f9d72449b10daf82ba11e
RSConcordia <renansconcordia@gmail.com>
2015-10-13 15:55:35 -0300

    add

--- js/index.js
+++ js/index.js
@@ -3,14 +3,33 @@
 		};
 	function list(){					
 					bluetoothSerial.list(function(devices){
-						devices.forEach(function(device){
-									var lista = "<a  href='#' onclick='connect()'>"+device.id+"</a><br>";
-									document.getElementById("dispositivos").innerHTML = lista;
-								}),
-									alert('Erro');
-								});
+					var i;
+					var lista = '';
+					for (i = 0; i < devices.length; i++) {
+						lista += "<a  href='#' onclick='connect()'>"+device.id[i] + "</a><br>";;
+					}
+
+					document.getElementById("dispositivos").innerHTML = lista;
 									
 	};
 	function connect(){
-		
+		var cars = ["BMW", "Volvo", "Saab", "Ford"];
+			var text = "";
+			var i;
+			for (i = 0; i < cars.length; i++) {
+				text += cars[i] + "<br>";
+			}
+
+			document.getElementById("dispositivos").innerHTML = text;
+
 	}
+	/*function list(){					
+					bluetoothSerial.list(function(devices){
+						devices.forEach(function(device){
+									
+									var lista = "";
+									lista = "<a  href='#' onclick='connect()'>"+device.id+"</a><br>";
+									document.getElementById("dispositivos").innerHTML = lista;
+								}),	});
+									
+	};*/
