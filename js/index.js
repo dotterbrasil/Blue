74dffa5df7d98b6fdfb5825f91257be61991072c
RSConcordia <renansconcordia@gmail.com>
2015-10-13 15:23:08 -0300

    add lista

--- js/index.js
+++ js/index.js
@@ -4,11 +4,7 @@
 	function list(){					
 					bluetoothSerial.list(function(devices){
 						devices.forEach(function(device){
-									var lista = document.createElement('a');										
-										lista.setAttribute("onclick", "onclick='connect()'");
-										lista.setAttribute('href', '#');
-										lista.appendChild(device.id);	
-									/*lista = "<a  href='#' onclick='connect()'>"+device.id+"</a><br>";*/
+									var lista = "<a  href='#' onclick='connect()'>"+device.id+"</a><br>";
 									document.getElementById("dispositivos").innerHTML = lista;
 								}),
 									alert('Erro');
