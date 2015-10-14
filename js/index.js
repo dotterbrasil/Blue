ac7c88f2c081d4bf17cfa57363221d80b0b3b985
RSConcordia <renansconcordia@gmail.com>
2015-10-14 10:36:09 -0300

    alert

--- js/index.js
+++ js/index.js
@@ -6,22 +6,29 @@
 			alert('Falha na conexao.');
 			}
 		};
-	function list(){ 
+	function list(){ alert();
 					var lista = "";
+					var i = "";
 					bluetoothSerial.list(function(devices){
-						devices.forEach(function(device){									
-									lista += "<a href='#' value='"+device.id+"' onclick='connect("+device.id+")'>"+device.name+"</a><br>";
+						devices.forEach(function(device){	
+									link = '"'+device.id+'"';
+									lista += "<a href='#' onclick='connect("+link+")'>"+device.name+"</a><br>";
 									document.getElementById("dispositivos").innerHTML = lista;
 								})
 							});			
 	};
-	function connect(device){
-		alert(device.id);
+	function connect(link){
+		alert(link);
 		//bluetoothSerial.connect(device, app.connectSuccess, app.connectFailure);
 	};
 	/*	function teste(){
-				id = '555';
-				name = 'nome';
-				lista = "<a href='#' value='"+id+"' onclick='connect("+id+")'>"+name+"</a><br>";
-				document.getElementById("dispositivos").innerHTML = lista;
+		
+		var cars = ["BMW", "Volvo", "Saab", "Ford"];
+				var lista = "";
+				var i;
+				for (i = 0; i < cars.length; i++) {
+					link = '"'+cars[i]+'"';
+					lista += "<a href='#' name='"+cars[i]+"' id='"+cars[i]+"' value='"+cars[i]+"' onclick='connect("+link+")'>"+cars[i]+"</a><br>";
+				}					
+					document.getElementById("dispositivos").innerHTML = lista;					
 	};*/
