f9f08ea89028a4b8c6915d77dd68777bc85c773a
RSConcordia <renansconcordia@gmail.com>
2015-10-15 14:55:41 -0300

    teste BC

--- js/index.js
+++ js/index.js
@@ -1,37 +1,36 @@
 	var app = {				
 			time: function(){	
-				alert('Dentro do loop');
-				document.getElementById("status").innerHTML = '...';
+				document.getElementById("status").innerHTML = 'Load...';
 				var link = localStorage.getItem("carro");
 				var status = localStorage.getItem("dispositivo");
 				
 				if(status == 'desconectado'){
-					alert('Dispositivo desconectado, tenta conexao');
+					alert('Dispositivo desconectado, tenta conexao!');
 					bluetoothSerial.connect(link, app.conectado, app.desconectado);
 				}else{
-					alert('Dispositivo conectado, verificaconexao conexao');
+					alert('Dispositivo conectado, verifica conexao!');
 					bluetoothSerial.isConnected(app.conectado, app.desconectado);	
 					}
 			},					
 			conectado: function(){
-				alert('Conectado');
-				document.getElementById("status").innerHTML = 'Continua conectado';
+				alert('On');
+				document.getElementById("status").innerHTML = 'Dispositivo Conectado';
 				localStorage.setItem("dispositivo", "conectado");
 				setTimeout(app.time, 3000);
 			},
 			desconectado: function(){
-				alert('Dispositivo desconectado!');
 				document.getElementById("status").innerHTML = "Dispositivo desconectado!";
 				localStorage.setItem("dispositivo", "desconectado");
 				setTimeout(app.time, 3000);
 			},
 	};			
-	function list(){
-					document.getElementById("status").innerHTML = '';
+	function list(){alert('dentro da list');
 					var lista = "";
-					bluetoothSerial.list(function(devices) {
+					/*bluetoothSerial.list*/
+					BC.Bluetooth.GetPairedDevices(function(devices) {
 					devices.forEach(function(device) {
 						var link = '';
+							alert('dentro do device');
 							link = '"'+device.id+'"';
 							lista += "<a href='#' onclick='connect("+link+")'>"+device.name+"</a><br>";
 							document.getElementById("dispositivos").innerHTML = lista;
