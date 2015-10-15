942f6d8474807a787b919507de99a1e52ea01f1f
RSConcordia <renansconcordia@gmail.com>
2015-10-15 08:45:48 -0300

    teste connect android to IOs

--- js/index.js
+++ js/index.js
@@ -2,39 +2,44 @@
 			connectSuccess: function(){
 			status = "Conectado.<br>";
 			document.getElementById("status").innerHTML = status;
+			alert('Conectado');
 			},
 			connectFailure: function(){
-			//status = "Falha na conexao.<br>";
-			//document.getElementById("status").innerHTML = status;
+			status = "Falha na conexao.<br>";
+			document.getElementById("status").innerHTML = status;
 			alert("Error");
 			},
 	};		
-		function buscar(){ 
+	function buscar(){ 
+		var lista = '';
 		bluetoothSerial.discoverUnpaired(function(devices){
 			devices.forEach(function(device){
 				var link = '';
 				link = '"'+device.id+'"';
 				lista += "<a href='#' onclick='connect("+link+")'>"+device.name+"</a><br>";
+				//lista += device.id+";";
 				document.getElementById("dispositivos").innerHTML = lista;
+				//document.getElementById("hidden").innerHTML = lista;
 				})								
-		}, alert("Error #3"));		
-		
+		}, alert("Error #3"));				
 	};
 	function list(){ 
 					var lista = "";
-					bluetoothSerial.list(function(devices) {
+					bluetoothSerial.list(function(devices) { //---- bluetoothSerial.list
 					devices.forEach(function(device) {
 						var link = '';
 							link = '"'+device.id+'"';
-							//link = device.id;
-							lista += "<a href='#' onclick='connect("+link+")'>"+device.name+"</a><br>";
+							lista += "<a href='#' onclick='connect("+link+")'>"+device.id+"</a><br>";
 							document.getElementById("dispositivos").innerHTML = lista;
 					})
 				});		
 	};
 	function connect(link){ 
-		document.getElementById("dispositivos").innerHTML = "";
-		document.getElementById("status").innerHTML = link;
+	document.getElementById("dispositivos").innerHTML = "";
+	document.getElementById("dispositivos").innerHTML = link;
+	bluetoothSerial.connect(link, app.connectSuccess, app.connectFailure);
+	
+	/*	
 		var string = document.getElementById("hidden").value;
 									
 				var mac = string.split(";");		
@@ -45,8 +50,7 @@
 					if(mac[contador] == link){
 							alert("Conectado");
 					}
-				}
-													
+				}*/
 	};
 	
 	
